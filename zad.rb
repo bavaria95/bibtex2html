421004dc9b249c9fffde1ae506568581ad84c2d0
bavaria95 <bavaria95@gmail.com>
2014-11-05 23:02:54 +0100

    wraped all to 2 classes

--- zad.rb
+++ zad.rb
@@ -1,10 +1,61 @@
-f = File.open('test.bib', 'r')
-text = ''
-f.each_line {|l| text += l}
+class Record
+  def initialize(fields, type, tag)
+    @filds = fields
+    @type = type
+    @tag = tag
+  end
 
-text.gsub!(/{\noopsort{\d{4}\w}}/, '')
+  def self.parse_data(text)
+    fields = Hash.new
+    entry_type, tag = '', ''
+    text.scan(/@(\w+){(.*),/){|x, y| entry_type = x; tag = y}
+    text.scan(/\s*(\w+)\s*=[{'" ](.+)[}'", ],?\s*/){|x, y| fields[x] = y}
+    (fields.each_value {|value| value.gsub!(/"/, '')}).delete('key')
+    return fields, entry_type, tag
+  end
 
-a = []
-text.scan(/@\w+{.*,\n[^@]*}/){|x| a << x}
+  def to_s
 
-puts a.length
+  end
+
+end
+
+class Records
+  def initialize
+    @records = []
+    # read
+    parse_entries(read).each do |record|
+      @records << Record.new(*Record.parse_data(record))
+    end
+  end
+
+  def ret_rec
+    @records
+  end
+
+  private
+  def read
+    f = File.open('test.bib', 'r')
+    text = ''
+    f.each_line {|l| text += l}
+    f.close
+    text
+  end
+
+  def parse_entries(text)
+    records = []
+    text.scan(/@\w+{.*,\n[^@]*}/){|x| records << x}
+    records
+  end
+
+
+end
+
+# text.gsub!(/{\noopsort{\d{4}\w}}/, '')
+r = Records.new
+puts r.ret_rec
+
+# a = []
+# text.scan(/@\w+{.*,\n[^@]*}/){|x| a << x}
+
+# puts a.length
